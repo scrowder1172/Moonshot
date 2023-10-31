0055494c661afa2e9b3c6fbd54c7d5cce2454db9
Scott Crowder <scrowder@gmail.com>
2023-10-31 15:49:42 -0500

    Build navigation links

--- Moonshot/TutorialFiles/NavigationLinkExample.swift
+++ Moonshot/TutorialFiles/NavigationLinkExample.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct NavigationLinkExample: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            NavigationLink {
+                Text("Detail View")
+            } label: {
+                VStack {
+                    Text("This is the label")
+                    Text("So is this")
+                    Image(systemName: "face.smiling")
+                }
+                .font(.largeTitle)
+            }
+            .navigationTitle("SwiftUI")
+            List {
+                ForEach(0..<100) {
+                    NavigationLink("Item \($0)") {
+                        List {
+                            ForEach(0..<100) {
+                                NavigationLink("SubItem \($0)") {
+                                    Text("This is the sub item's sub item")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
