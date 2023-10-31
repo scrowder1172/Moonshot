996ac1548c6df8cd49925e14ff651ec740ff2686
Scott Crowder <scrowder@gmail.com>
2023-10-31 16:03:21 -0500

    Create codable protocol example

---
+++ Moonshot/TutorialFiles/CodableProtocolExample.swift
@@ -0,0 +1,18 @@
+//
+//  CodableProtocolExample.swift
+//  Moonshot
+//
+//  Created by SCOTT CROWDER on 10/31/23.
+//
+
+import SwiftUI
+
+struct CodableProtocolExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodableProtocolExample()
+}
