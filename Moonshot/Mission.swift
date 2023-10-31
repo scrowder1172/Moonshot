18e7892d94bf7f7ae895345ac885fcf063017ad8
Scott Crowder <scrowder@gmail.com>
2023-10-31 18:08:55 -0500

    create structs for mission data

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct CrewRole: Codable {
+    let name: String
+    let role: String
+}
+
+struct Mission: Codable, Identifiable {
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
