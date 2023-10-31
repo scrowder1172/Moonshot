df18692689694d43c023ddec2bdec613e09d0267
Scott Crowder <scrowder@gmail.com>
2023-10-31 14:11:17 -0500

    create scroll view example

---
+++ Moonshot/TutorialFiles/ScrollViewExample.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewExample.swift
+//  Moonshot
+//
+//  Created by SCOTT CROWDER on 10/31/23.
+//
+
+import SwiftUI
+
+struct ScrollViewExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewExample()
+}
