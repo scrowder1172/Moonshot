8c0ab7f57b4f5bcc952b73097e466d9631c1aaee
Scott Crowder <scrowder@gmail.com>
2023-10-31 17:56:31 -0500

    create Bundle extension to decode json data file

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let fileData = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode([String: Astronaut].self, from: fileData) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return decodedData
+    }
+}
