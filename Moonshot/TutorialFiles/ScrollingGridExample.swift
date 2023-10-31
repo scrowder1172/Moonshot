b6e7502b9f83533d6c36a92d5dc57df064264310
Scott Crowder <scrowder@gmail.com>
2023-10-31 16:43:33 -0500

    create scrolling grid example

---
+++ Moonshot/TutorialFiles/ScrollingGridExample.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollingGridExample.swift
+//  Moonshot
+//
+//  Created by SCOTT CROWDER on 10/31/23.
+//
+
+import SwiftUI
+
+struct ScrollingGridExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollingGridExample()
+}
