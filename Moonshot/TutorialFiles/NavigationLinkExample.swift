9990fc998c10fe75ab6c0b917b112d048bb09bcc
Scott Crowder <scrowder@gmail.com>
2023-10-31 15:32:54 -0500

    Create NavigationLink examples

---
+++ Moonshot/TutorialFiles/NavigationLinkExample.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationLinkExample.swift
+//  Moonshot
+//
+//  Created by SCOTT CROWDER on 10/31/23.
+//
+
+import SwiftUI
+
+struct NavigationLinkExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationLinkExample()
+}
