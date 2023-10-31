d3f5dee428bd167a736361f5f33ac554b977d30a
Scott Crowder <scrowder@gmail.com>
2023-10-31 13:22:52 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by SCOTT CROWDER on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
