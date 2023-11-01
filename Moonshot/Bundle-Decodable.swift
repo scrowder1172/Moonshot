b5675170e44087905a8b96723af54ba9fe514f28
Scott Crowder <scrowder@gmail.com>
2023-10-31 21:18:27 -0500

    Add generics

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+    func decode<T: Codable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
@@ -19,7 +19,7 @@ extension Bundle {
         
         let decoder = JSONDecoder()
         
-        guard let decodedData = try? decoder.decode([String: Astronaut].self, from: fileData) else {
+        guard let decodedData = try? decoder.decode(T.self, from: fileData) else {
             fatalError("Failed to decode \(file) from bundle.")
         }
         
