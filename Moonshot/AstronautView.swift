b90ac27a38b715527735d8ea16f3750295efccb0
Scott Crowder <scrowder@gmail.com>
2023-11-01 16:35:14 -0500

    create file

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by SCOTT CROWDER on 11/1/23.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautView()
+}
