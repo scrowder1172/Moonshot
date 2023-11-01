a11a22a721bfb945bc309228d9cf6cb7218d2d8b
Scott Crowder <scrowder@gmail.com>
2023-11-01 10:39:22 -0500

    create hex code color initializer and custom dark and light colors

--- Moonshot/Color-Theme.swift
+++ Moonshot/Color-Theme.swift
@@ -5,4 +5,40 @@
 //  Created by SCOTT CROWDER on 11/1/23.
 //
 
-import Foundation
+import SwiftUI
+
+
+extension ShapeStyle where Self == Color {
+    init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+    
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+    
+    static var hexDarkBackground: Color {
+        // hex code representation of the darkBackground defined above
+        Self(hex: "#1A1A33")
+    }
+    
+    static var hexLightBackground: Color {
+        // hex code representation of the lightBackground defined above
+        Self(hex: "#333344")
+    }
+}
