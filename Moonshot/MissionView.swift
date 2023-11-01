939b516e8c47dd73301ea9dab012be4a2d9dc7b5
Scott Crowder <scrowder@gmail.com>
2023-11-01 12:09:29 -0500

    create UI for mission details

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct MissionView: View {
+    let mission: Mission
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) {width, axis in
+                        width * 0.6
+                    }
+                    .padding(.top)
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
     }
 }
 
 #Preview {
-    MissionView()
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
 }
