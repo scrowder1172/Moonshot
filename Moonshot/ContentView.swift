960fae390d8de389670214286ada645677f2fafe
Scott Crowder <scrowder@gmail.com>
2023-10-31 21:18:50 -0500

    create missions property

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
         Text(String(astronauts.count))
+        Text(String(missions.count))
     }
 }
 
