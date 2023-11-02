2df863ab84aad8ec373ac2e989e571b53da0962d
Scott Crowder <scrowder@gmail.com>
2023-11-02 11:00:19 -0500

    Moved custom views to seperate file

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -58,30 +58,6 @@ struct ContentView: View {
     }
 }
 
-struct CustomButton: View {
-    let label: String
-    let action: () -> Void
-    var body: some View {
-        Button {
-            action()
-        } label: {
-            Text(label)
-        }
-        .frame(width: 200, height: 30)
-        .background(.blue)
-        .foregroundStyle(.white)
-        .bold()
-        .clipShape(.rect(cornerRadius: 20))
-    }
-}
-
-struct HorizontalLine: View {
-    var body: some View {
-        Rectangle()
-            .frame(height: 1)
-    }
-}
-
 
 #Preview {
     ContentView()
--- Moonshot/CustomViews.swift
+++ Moonshot/CustomViews.swift
@@ -5,4 +5,28 @@
 //  Created by SCOTT CROWDER on 11/2/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomButton: View {
+    let label: String
+    let action: () -> Void
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Text(label)
+        }
+        .frame(width: 200, height: 30)
+        .background(.blue)
+        .foregroundStyle(.white)
+        .bold()
+        .clipShape(.rect(cornerRadius: 20))
+    }
+}
+
+struct HorizontalLine: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 1)
+    }
+}
